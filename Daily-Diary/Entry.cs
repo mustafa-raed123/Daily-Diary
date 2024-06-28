b36f44961d847f152776e3ec064bee9a0a9eda25
mura123+ <mwsym1898@gmail.com>
2024-06-28 14:53:26 +0300

    add DiaryManegers

---
+++ Daily-Diary/Entry.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Diary
+{
+    public class Entry
+    {
+        public string Time { get; set; }
+        public string Content { get; set; }
+
+
+        public void CheckDate()
+        {
+
+            bool IsCorrect = false;
+                    Console.Write("Enter a date (e.g., YYYY-M-D): ");
+            while (!IsCorrect)
+            {
+                try
+                {
+                    string dates = Console.ReadLine();
+                    DateTime parsedDate = DateTime.ParseExact(dates, "yyyy-M-d", CultureInfo.CurrentCulture);
+                     Time = parsedDate.ToString("yyyy-M-d");
+                 
+                    IsCorrect = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("The date is not valid. Enter a valid date such as (2012-2-23).");
+                }
+            }
+        }
+        public void CheckContent()
+        {
+
+            Console.Write("Enter a Diary:");
+            
+            bool IsCorrect = false;
+            while (!IsCorrect)
+            {
+              
+                
+                    string diary = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(diary))
+                {
+                    Content = diary;
+                    IsCorrect = true;
+                }
+                else { Console.Write("The diary is empty. Please enter a correct diary entry: "); 
+                }
+
+                
+                
+                
+               
+            }
+        }
+
+
+    }
+}
--- Daily-Diary/Program.cs
+++ Daily-Diary/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Daily_Diary
+﻿using System.Globalization;
+
+namespace Daily_Diary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+          
+            DailyDiary diary = new DailyDiary();
+             diary.CheckContent();
+
         }
     }
 }
